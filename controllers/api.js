6b565439a21a889572aea4597574f1b52eaab873
Manasa Akula <s560998@nwmissouri.edu>
2023-04-13 21:44:41 -0500

    made changes

--- controllers/api.js
+++ controllers/api.js
@@ -2,7 +2,7 @@
 exports.api = function(req, res) {
     res.write('[');
     res.write('{"resource":"shoes", ');
-    res.write(' "verbs":["GET","PUT", "DELETE"] ');
+    res.write(' "verbs":["GET","PUT","POST", "DELETE"] ');
     res.write('}');
     res.write(']')
     res.send();
--- routes/shoes.js
+++ routes/shoes.js
@@ -2,7 +2,7 @@ var express = require('express');
 const shoes_controlers= require('../controllers/shoes');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users listing. 
 class shoes{
     constructor(shoes_type, shoes_name, shoes_cost){
         this.shoes_type=shoes_type;
@@ -11,13 +11,13 @@ class shoes{
     }
 }
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
     let s1= new shoes('Knee-high','Nike',6000);
     let s2= new shoes('Earth shoe','Adidas',4000);
     let s3= new shoes('Flip-flops','Reebok',5000);
   res.render('shoes', { title: 'Search Results Shoes',shoes: [s1,s2,s3] });
-});
+});*/
 
 router.get('/', shoes_controlers.shoes_view_all_Page );
 module.exports = router;
