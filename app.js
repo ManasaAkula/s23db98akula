57c72fb840fa9e824d4c174df94edf46a31070f0
Manasa Akula <s560998@nwmissouri.edu>
2023-04-24 12:57:23 -0500

    made changes

--- app.js
+++ app.js
@@ -48,6 +48,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -57,13 +65,7 @@ app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
- }));
- app.use(passport.initialize());
- app.use(passport.session());
+
 
  // passport config
 // Use the existing connection
--- routes/shoes.js
+++ routes/shoes.js
@@ -22,6 +22,18 @@ router.get('/', function(req, res, next) {
 router.get('/', shoes_controlers.shoes_view_all_Page );
 router.get('/detail', shoes_controlers.shoes_view_one_Page);
 router.get('/create', shoes_controlers.shoes_create_Page);
-router.get('/update', shoes_controlers.shoes_update_Page);
+//router.get('/update', shoes_controlers.shoes_update_Page);
 router.get('/delete', shoes_controlers.shoes_delete_Page);
 module.exports = router;
+
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+    /* GET update costume page */
+    
+router.get('/update', secured,shoes_controlers.shoes_update_Page);
+    
